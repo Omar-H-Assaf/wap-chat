bbef2e166e8642c526ee6977515740ed96de4430
Omar-H-Assaf <omar.assaf@hearst.com>
2023-03-19 01:03:56 -0500

    Add content

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+var path = require("path")
 const app = express();
-var userRoutes = require('./routes/admin.js');
+var loginRoutes = require('./routes/login.js');
 
 app.listen(80, () => {
     console.log('Your Server is running on 80');
@@ -9,7 +10,8 @@ app.listen(80, () => {
 
 
 //Read the parameters from post request
+app.use(express.static(path.join(__dirname,"styles")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/user',userRoutes);
+app.use('/login',loginRoutes);
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+var path = require("path");
 
 router.get("/",function(req,res,next){
-    res.send("User Routes")
-})
+    res.sendFile(path.join(__dirname,"../","views","login.html"));
+});
 
 module.exports = router;
